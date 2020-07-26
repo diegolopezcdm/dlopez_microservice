ab8ca167cc4b60995726d56343a8dd13de226b5a
diegol <diegolopezcdm@gmail.com>
2020-07-25 22:48:45 -0500

    added register and discover

---
+++ backend/spring-cloud-ms-register-discover/src/test/java/com/dlopez/springcloudmsregisterdiscover/SpringCloudMsRegisterDiscoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlopez.springcloudmsregisterdiscover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMsRegisterDiscoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
