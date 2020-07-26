2da2018a1f3aa4597f53814e4e2d819edf492aea
diegol <diegolopezcdm@gmail.com>
2020-07-25 22:42:21 -0500

    added config server

---
+++ backend/spring-cloud-ms-config-server/src/test/java/com/dlopez/demo/SpringCloudMsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlopez.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
